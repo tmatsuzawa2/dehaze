d70c2d1f5655a6a01451a749a9151960075443ec
tmatsuzawa2 <36981191+tmatsuzawa2@users.noreply.github.com>
2018-12-10 19:46:45 -0600

    Update get_radiance.m

--- get_radiance.m
+++ get_radiance.m
@@ -6,8 +6,8 @@ function radiance = get_radiance(image, depth, atmosphere)
 
 rep_atmosphere = repmat(reshape(atmosphere, [1, 1, 3]), m, n);
 
-max_transmission = repmat(max(depth, 0.1), [1, 1, 3]);
+max_depth = repmat(max(depth, 0.1), [1, 1, 3]);
 
-radiance = ((image - rep_atmosphere) ./ max_transmission) + rep_atmosphere;
+radiance = ((image - rep_atmosphere) ./ max_depth) + rep_atmosphere;
 
 end
